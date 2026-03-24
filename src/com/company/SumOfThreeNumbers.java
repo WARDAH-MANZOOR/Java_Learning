0f75f74958efd91f1b10846d3a6c15514f953251
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-03-24 11:57:18 +0500

    added sum of three numbers code

---
+++ src/com/company/SumOfThreeNumbers.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class SumOfThreeNumbers {
+    public static void main(String[] args){
+
+        System.out.println("Sum of three numbers");
+        int num1 = 5;
+        int num2 = 3;
+        int num3 = 6;
+
+        int sum =  num1 + num2 + num3;
+        System.out.println(sum); // println prints new line , only print without ln prints in same line
+
+    }
+}
