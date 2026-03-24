fc378b81509dd3ef1f8c5643925e38ffd36700ee
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-03-24 12:36:06 +0500

    added literals code

---
+++ src/com/company/Literals.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Literals {
+    public static void main(String[] args) {
+        // A CONSTANT VALUE WHICH CAN BE ASSIGNED TO THE VARIABLE IS CALLED AS A LITERAL
+
+        // PRIMITIVE DATA TYPES: INTEGRAL(INT), FLOAT, CHAR, BOOLEAN
+        // FORMULA FOR RANGE CALCULATION THAT HOW MUCH DATA TAKES BY DATA TYPE
+        // [-{2^n(no.of bytes)*8(no. of bits in byte)}] to [{2^n(no.of bytes)*8(no. of bits in byte)}]  -1
+        // INT: INT(takes 4 bytes) , SHORT(takes 2 bytes) , LONG(takes 8 bytes) , BYTE(takes 1 byte)
+        // FLOAT : FLOAT(denoted by f---(takes 4 bytes) ), DOUBLE(denoted by D --- (takes 8 bytes) )
+        // CHAR: DENOTED BY '' ---- (takes 2 byte)
+        // BOOLEAN: TRUE/FALSE
+
+        byte age = 34;
+        int age1 = 59;
+        short age2 = 32;
+        long age3 = 3472382928982L;
+        char character = 'A';
+        float f = 3.1f;
+        double d = 3.44422; // or 3.44422D
+        String name = "Wardah";
+
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(character);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(name);
+
+
+
+
+
+    }
+}
