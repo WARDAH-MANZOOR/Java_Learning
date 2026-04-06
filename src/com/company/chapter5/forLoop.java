f9c126cdbdd0eee29f5a73dd8816c04c7ca37e5b
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-06 16:54:28 +0500

    added for loop

---
+++ src/com/company/chapter5/forLoop.java
@@ -0,0 +1,20 @@
+package com.company.chapter5;
+// FOR LOOP --- A for loop is usually used to execute a piece of code for specific number of times
+public class forLoop {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+
+        // odd numbers:
+        int n = 20;
+        for (int a=0;a<n;a++){
+            System.out.println((2*a)+1);
+        }
+        //even numbers
+        for (int a=0;a<n;a++){
+            System.out.println(2*a);
+        }
+
+    }
+}
