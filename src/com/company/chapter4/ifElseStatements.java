fc874ec450314141a387f575aaa3e9f644d43890
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-06 13:05:25 +0500

    if else statements added

--- src/com/company/chapter4/ifElseStatements.java
+++ src/com/company/chapter4/ifElseStatements.java
@@ -2,6 +2,15 @@ package com.company.chapter4;
 
 public class ifElseStatements {
     public static void main(String[] args) {
+        int age = 19;
+        if(age>18){
+            System.out.println("You are allowed to drive");
+
+        }
+        else{
+            System.out.println("You are not allowed to drive");
+        }
+
 
     }
 }
