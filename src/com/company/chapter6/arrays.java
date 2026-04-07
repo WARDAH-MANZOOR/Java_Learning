ec52a2f1c921eb7b818799735f8a4a6893e3d7c4
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-07 12:52:00 +0500

    updated arrays file

--- src/com/company/chapter6/arrays.java
+++ src/com/company/chapter6/arrays.java
@@ -1,5 +1,14 @@
 package com.company.chapter6;
+// ARRAY: Array is a collection of similar types of data, three ways to create array
+// option 1---declaration in 1 line and memory allocation in second line
+// int [] marks; --- declaration
+// new int [10]; ---- memory allocation
 
+// option 2 --- declaration +memory allocation single line
+// int [] marks = new int[10]
+
+// option 3 --- declare +initialize
+//int [] marks = {67,44,33,78,99};
 public class arrays {
     public static void main(String[] args) {
         int [] marks = new int[10]; // array for 10 elements
