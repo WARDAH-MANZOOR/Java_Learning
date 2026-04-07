d2a536398e8ed584c6638305bdab44bcab043912
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-07 12:43:25 +0500

    added array code

--- src/com/company/chapter6/arrays.java
+++ src/com/company/chapter6/arrays.java
@@ -2,6 +2,20 @@ package com.company.chapter6;
 
 public class arrays {
     public static void main(String[] args) {
+        int [] marks = new int[10]; // array for 10 elements
+        marks[0]=44;
+        marks[1]=55;
+        marks[2]=66;
+        marks[3]=77;
+        marks[4]=88;
+        marks[5]=99;
+        marks[6]=99;
+        marks[7]=99;
+        marks[8]=99;
+        marks[9]=99;
+        System.out.println(marks[0]);
+
+
 
     }
 }
