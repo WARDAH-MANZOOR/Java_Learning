1f884c0831db752ccdf98aacd476aa8631462fa9
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-13 13:53:37 +0500

    added practice set questions for chapter 8

---
+++ src/com/company/chapter8/practiceSet.java
@@ -0,0 +1,14 @@
+package com.company.chapter8;
+
+public class practiceSet {
+    public static void main(String[] args) {
+        System.out.println("PRACTICE QUESTIONS FOR CHAPTER 8");
+        System.out.println("QUETSION # 01: Create a class employee with following properties and methods:\n salary (property: int) \n getSalry (method: returning int)\n name (property: string)\n getName (method: returning string)\n setName (method: changing name)");
+        System.out.println("QUETSION # 02: Create a class cellpone with methods to print \"ringing\",\"vibrating\" etc ");
+        System.out.println("QUETSION # 03: Create a class square with a method to initialize its sides, calculating area , its perimeter etc");
+        System.out.println("QUETSION # 04: Create a class rectangle with a method to initialize its sides, calculating area , its perimeter etc ");
+        System.out.println("QUETSION # 05: Create a class Tommy for rockstar games capable of hitting(print hitting), running, firing etc");
+        System.out.println("QUETSION # 06: Create a class circle with a method to initialize its sides, calculating area , its perimeter etc ");
+
+    }
+}
