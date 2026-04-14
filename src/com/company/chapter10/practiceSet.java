58c61cb8ca289b6354edc5961b2df03fb50b7874
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-14 17:12:02 +0500

    added practice questions

---
+++ src/com/company/chapter10/practiceSet.java
@@ -0,0 +1,12 @@
+package com.company.chapter10;
+
+public class practiceSet {
+    public static void main(String[] args) {
+        System.out.println("PRACTICE QUESTIONS FOR CHAPTER 10");
+        System.out.println("QUESTION # 01: Create a class cirle and use inheritance to create another class cylinder from it");
+        System.out.println("QUESTION # 02: Create a class REctangle and use inheritance to create another class cuboid. Try to keep it as close to real world as possible ");
+        System.out.println("QUESTION # 03: Create methods for area and volumne in question 1");
+        System.out.println("QUESTION # 04: Create methods for are and volume in quetsion 2, also create getters and setters");
+        System.out.println("QUESTION # 05: ");
+    }
+}
