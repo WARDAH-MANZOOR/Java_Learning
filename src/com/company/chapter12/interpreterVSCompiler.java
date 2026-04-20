da0be85918ca83230cc9d4502fc7247fb59440b4
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-20 12:11:58 +0500

    added chapter 12 folder

---
+++ src/com/company/chapter12/interpreterVSCompiler.java
@@ -0,0 +1,6 @@
+package com.company.chapter12;
+// interpreter: Interpreter translates one statment at a time into machine code
+// compiler: compiler scans the entire program and transaltes  whole ofit into machine code
+// java is  a hybrid language both compiler and interpreter
+public class interpreterVSCompiler {
+}
