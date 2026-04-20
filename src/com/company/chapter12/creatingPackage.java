2f275e5263e67a7ad2897c8abf45371438dfa6a0
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-20 13:27:47 +0500

    creating pacakges

---
+++ src/com/company/chapter12/creatingPackage.java
@@ -0,0 +1,30 @@
+package com.company.chapter12;
+// acha ab ye hai ke hum aik pacakge create karna chhate hain mene interpreter vs cpompiler wali file me bataya tha ke
+// hum vs code me apni java ki file ko jab compile karte hain tou class file banjati hai ab agr hum poora package
+// hi compile karna  chahen jisme sari files hon java ki usko compile karne ke liye hum vs code us ekarskate hain
+// pehle jab hum aik file banate tahy jese mene bataya tah Warad.java ki file banai thi usko compile kia tha
+// javac Wardah.java ki command se phr class file bangaye thi us class file ko execeute kia tah java Wardah se
+//ab ye hai ke hum ye chharahe ke multiple java ki files ko hum aik pacakge me dalen aur phr usko compile karen
+// jistarah abhi humara project hai ke com.company me sari files aur flders added hain, acha tou vs code me ye karne
+// liye humne aik pacakge banaya codeWithHarry ke name se package aese ke jis jis file ko humne add karna tha us pacakge
+// me humne aese likha package codeWithHarry uske baad ab ye sari file tou us pacakge me chali agye ab usko compile bhi tou
+// karna ahi jisse sari class files bhi codeWithHarry ke pacakege me bane suke liye hum ye commands chhals akte hain
+// aik tou ye ke jab hum aik specific ko pacakge me dalne ke liye compile karraho hon tou hum ye command chalainge
+// javac -d .Wardah.java usse pacakge banjaaiga aur uske iski class file add hojaigi aur agr humne sari files ai sath
+// hi compile aur add karni hain tou uske liye javac -d . *.ja isse sari files aik sath hi compile hojaigi jo bhi jis bhi
+//pacakge me hai us pacakge me add hojaingi tou matlab ye ke agr 10 files me pacakge codeWithHarry hai aur baki ki 5 me
+// codeWithHarry1 tou ye 2 alag apacakges hogaye tou ari files compile karne wali command chalane par sari class files
+// apne apne pacakges me chali jaaingi
+// usage: acha ab compile tou hogaya par use bhi tou karna hai suke liye hum dosri file jisme humne wo pacakge
+// jisko use karna hai import karte ahin example: import codeWithHarry.gym (codeWithHarry ke pacakge ke andar gym ka pacakge
+// uske andar ki files us ekarni hain is tarah hum apne custom pacakes ko banakar import karte hain
+
+
+
+
+
+public class creatingPackage {
+    public static void main(String[] args) {
+
+    }
+}
