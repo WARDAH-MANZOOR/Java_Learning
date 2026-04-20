90f61a11d6144f27d401ffd5f8533e1164c80832
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-20 15:30:54 +0500

    added chapter 13

---
+++ src/com/company/chapter13/multiThreading.java
@@ -0,0 +1,8 @@
+package com.company.chapter13;
+// Multithreading and multiprocessing(bahut sare processes aik sath chalana) both are used to achieve multitasking
+// withi a process multitasking karne ke liye hum threads use karte hain threads process ka hissa hota hai
+public class multiThreading {
+    public static void main(String[] args) {
+
+    }
+}
